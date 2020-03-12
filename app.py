7cee49d7004a6a5a155fbf9eb4f46a2eb3f81fd8
bhanuka1998 <bhanuka06@gmail.com>
2020-03-12 15:00:39 +0530

    implemented and finalized all APIs and website design

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, jsonify
 from pymongo import MongoClient
 from bson import json_util
+import requests
 
 app = Flask(__name__)
 
@@ -10,13 +11,58 @@ collection = db.to_dos
 
 @app.route('/')
 def index():
-    user = {'username': 'Bhanuka'}
-    return render_template('index.html', title='Homepage', user=user)
+    user = {'username': 'Thilina'}
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/elements')
+def elements():
+    return render_template('elements.html')
+
+@app.route('/single-blog')
+def single_blog():
+    return render_template('single-blog.html')
+
+@app.route('/track')
+def track():
+    return render_template('track.html')
+
+@app.route('/fb')
+def fb():
+    return render_template('fb.html')
+
 
 @app.route('/view', methods=['GET'])
 def get_todos():
     to_dos = list(collection.find())
     return json_util.dumps(to_dos)
+
+@app.route('/search')
+
+def search():
+
+    recipe = request.args.get('a', 0, type=str)
+
+    response = requests.get(f"https://searchly.asuarez.dev/api/v1/song/search?query={recipe}")
+
+    try:
+        response = response.json()["response"]["results"][0]
+    except:
+        response = {"name": "Invalid search. Please try again."}
+    return jsonify(result=response) 
+
+
 
 
 @app.route('/add', methods=['POST'])
@@ -25,16 +71,17 @@ def add_todo():
         new_todo = request.get_json()
         name = new_todo['name']
         description = new_todo['description']
-        time = new_todo['time']
+        timer = new_todo['timer']
 
         collection.insert_one({
-            "name": name, 
+            "name": name,
             "description": description,
-            "time": time
+            "timer": timer
         })
-        return redirect('/view')
+
+    return redirect('/view')
+
 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='8000', debug=True)
-
